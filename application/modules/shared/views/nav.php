82c4abb9782c294768d9d85f4e4f2ad81116969c
Joseph Mark Anthony Huelgas <huelgasjcowork@gmail.com>
2017-12-09 01:14:05 +0800

    Changed Nav Color!

--- application/modules/shared/views/nav.php
+++ application/modules/shared/views/nav.php
@@ -8,19 +8,19 @@
 		</a>
 	</div>
 	<div class="collapse navbar-collapse" id="navbar-collapse-01">
-		<ul class="nav navbar-nav navbar-nav-c">
+		<ul class="nav navbar-nav">
 
 			<?php if (isset($siteData) || (isset($page) && $page == 'newPage')) : ?>
 
 				<?php if (isset($siteData)) : ?>
-					<li class="active active-c">
-						<a class="a-c"><span class="fui-home"></span> <span id="siteTitle"><?php echo $siteData['site']->sites_name; ?></span></a>
+					<li class="active">
+						<a><span class="fui-home"></span> <span id="siteTitle"><?php echo $siteData['site']->sites_name; ?></span></a>
 					</li>
 				<?php endif; ?>
 
 				<?php if (isset($page) && $page == 'newPage') : ?>
-					<li class="active active-c">
-						<a class="a-c"><span class="fui-home"></span> <span id="siteTitle"><?php echo $this->lang->line('newsite_default_title'); ?></span> </a>
+					<li class="active">
+						<a><span class="fui-home"></span> <span id="siteTitle"><?php echo $this->lang->line('newsite_default_title'); ?></span> </a>
 					</li>
 				<?php endif; ?>
 
@@ -41,22 +41,22 @@
 					}
 					?>
 
-					<li><a class="a-c" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" id="backButton"><span class="fui-arrow-left"></span> <?php echo $this->lang->line($t); ?></a></li>
+					<li><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" id="backButton"><span class="fui-arrow-left"></span> <?php echo $this->lang->line($t); ?></a></li>
 
 				<?php else: ?>
 
-					<li><a class="a-c" href="sites" id="backButton"><span class="fui-arrow-left"></span> <?php echo $this->lang->line('nav_goback_users'); ?></a></li>
+					<li><a href="sites" id="backButton"><span class="fui-arrow-left"></span> <?php echo $this->lang->line('nav_goback_users'); ?></a></li>
 
 				<?php endif; ?>
 
 			<?php else: ?>
 
-				<li <?php if (isset($page) && $page == "site") : ?>class="active"<?php endif; ?>><a href="sites"><span class="fui-windows"></span> <?php echo $this->lang->line('nav_sites'); ?></a></li>
-				<li <?php if (isset($page) && $page == "asset") : ?>class="active"<?php endif; ?>><a href="asset/images"><span class="fui-image"></span> <?php echo $this->lang->line('nav_imagelibrary'); ?></a></li>
+				<li <?php if (isset($page) && $page == "site") : ?>class=" active-c"<?php endif; ?>><a href="sites"><span class="fui-windows"></span> <?php echo $this->lang->line('nav_sites'); ?></a></li>
+				<li <?php if (isset($page) && $page == "asset") : ?>class=" active-c"<?php endif; ?>><a href="asset/images"><span class="fui-image"></span> <?php echo $this->lang->line('nav_imagelibrary'); ?></a></li>
 				<?php if ($this->session->userdata('user_type') == 'Admin') : ?>
-					<li <?php if (isset($page) && $page == "user") : ?>class="active"<?php endif; ?>><a href="user"><span class="fui-user"></span> <?php echo $this->lang->line('nav_users'); ?></a></li>
-					<li <?php if (isset($page) && $page == "package") : ?>class="active"<?php endif; ?>><a href="package"><span class="fui-list-bulleted"></span> <?php echo $this->lang->line('nav_packages'); ?></a></li>
-					<li <?php if (isset($page) && $page == "settings") : ?>class="active"<?php endif; ?>><a href="settings"><span class="fui-gear"></span> <?php echo $this->lang->line('nav_settings'); ?></a></li>
+					<li <?php if (isset($page) && $page == "user") : ?>class=" active-c"<?php endif; ?>><a href="user"><span class="fui-user"></span> <?php echo $this->lang->line('nav_users'); ?></a></li>
+					<li <?php if (isset($page) && $page == "package") : ?>class=" active-c"<?php endif; ?>><a href="package"><span class="fui-list-bulleted"></span> <?php echo $this->lang->line('nav_packages'); ?></a></li>
+					<li <?php if (isset($page) && $page == "settings") : ?>class=" active-c"<?php endif; ?>><a href="settings"><span class="fui-gear"></span> <?php echo $this->lang->line('nav_settings'); ?></a></li>
 				<?php endif; ?>
 
 			<?php endif; ?>
