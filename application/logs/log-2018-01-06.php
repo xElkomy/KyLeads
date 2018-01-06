b5825066ba5484807e6eb84939ba3ec1b7d7bad4
MeltitoVagallon <meltito.vagallon@gmail.com>
2018-01-06 18:38:35 +0800

    added take quiz functionality

---
+++ application/logs/log-2018-01-06.php
@@ -0,0 +1,72 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-01-06 04:05:54 --> Query error: Unknown column 'question_id' in 'field list' - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `question_id`, `create_at`) VALUES (1, 17, 1, 1515207953)
+ERROR - 2018-01-06 04:06:09 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:06:17 --> Query error: Unknown column 'question_id' in 'field list' - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `question_id`, `create_at`) VALUES (1, 17, 1, 1515207977)
+ERROR - 2018-01-06 04:07:14 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:07:36 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:07:48 --> Query error: Unknown column 'question_id' in 'field list' - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `question_id`, `create_at`) VALUES (1, 17, 1, 1515208068)
+ERROR - 2018-01-06 04:09:41 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:11:49 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:42:40 --> Query error: Column 'email' cannot be null - Invalid query: INSERT INTO `contacts` (`user_id`, `first_name`, `last_name`, `email`) VALUES (17, 'firstname', 'lastname', NULL)
+ERROR - 2018-01-06 04:44:33 --> Query error: Column 'email' cannot be null - Invalid query: INSERT INTO `contacts` (`user_id`, `first_name`, `last_name`, `email`) VALUES (2, 'firstname', 'lastname', NULL)
+ERROR - 2018-01-06 04:45:33 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:45:43 --> Query error: Column 'email' cannot be null - Invalid query: INSERT INTO `contacts` (`user_id`, `first_name`, `last_name`, `email`) VALUES (17, 'firstname', 'lastname', NULL)
+ERROR - 2018-01-06 04:46:11 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:46:24 --> Query error: Column 'email' cannot be null - Invalid query: INSERT INTO `contacts` (`user_id`, `first_name`, `last_name`, `email`) VALUES (1, 'firstname', 'lastname', NULL)
+ERROR - 2018-01-06 04:47:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:47:20 --> Query error: Column 'email' cannot be null - Invalid query: INSERT INTO `contacts` (`user_id`, `first_name`, `last_name`, `email`) VALUES (1, 'firstname', 'lastname', NULL)
+ERROR - 2018-01-06 04:47:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:49:23 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:49:42 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 04:51:47 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 06:45:45 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:03:22 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:04:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:49:09 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:50:13 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:53:21 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:54:30 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:55:26 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:55:49 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:56:21 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:57:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 07:58:07 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:01:50 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:04:23 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:05:06 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:06:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:14:25 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (1, 17, NULL)
+ERROR - 2018-01-06 08:20:32 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:34 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:42 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:44 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:45 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:47 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:49 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:51 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:51 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:53 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:20:55 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:26:26 --> Severity: error --> Exception: Too few arguments to function Takequiz_model::Addcontacts_results(), 2 passed in C:\xampp\htdocs\application\modules\quiz\controllers\Takequiz.php on line 87 and exactly 3 expected C:\xampp\htdocs\application\modules\quiz\models\Takequiz_model.php 132
+ERROR - 2018-01-06 08:30:00 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (1, 17, NULL)
+ERROR - 2018-01-06 08:34:44 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (2, 17, NULL)
+ERROR - 2018-01-06 08:36:04 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (3, 17, NULL)
+ERROR - 2018-01-06 08:38:14 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (4, 17, NULL)
+ERROR - 2018-01-06 08:39:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:43:26 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (5, 17, NULL)
+ERROR - 2018-01-06 08:44:27 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (6, 17, NULL)
+ERROR - 2018-01-06 08:46:08 --> Query error: Column 'outcome_id' cannot be null - Invalid query: INSERT INTO `contacts_results` (`contact_id`, `quiz_id`, `outcome_id`) VALUES (7, 17, NULL)
+ERROR - 2018-01-06 08:48:23 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:49:29 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:50:22 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:51:28 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:52:10 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:53:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 08:54:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\takequiz\viewquiz.php 28
+ERROR - 2018-01-06 10:00:57 --> Severity: Notice --> Undefined variable: packages C:\xampp\htdocs\application\modules\shared\views\modal_account.php 102
+ERROR - 2018-01-06 10:00:57 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\shared\views\modal_account.php 102
+ERROR - 2018-01-06 10:01:33 --> Severity: Notice --> Undefined variable: packages C:\xampp\htdocs\application\modules\shared\views\modal_account.php 102
+ERROR - 2018-01-06 10:01:33 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\shared\views\modal_account.php 102
+ERROR - 2018-01-06 11:23:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\templates\template_preview.php 28
+ERROR - 2018-01-06 11:23:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\application\modules\quiz\views\templates\template_preview.php 188
